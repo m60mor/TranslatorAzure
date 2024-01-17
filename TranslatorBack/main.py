3298af3c02a9f3217b8f433edad23510d1ba1a45
ArseniMokhau <arsenijm140@gmail.com>
2024-01-18 01:53:20 +0300

    Added Document Intelligence integration

--- TranslatorBack/main.py
+++ TranslatorBack/main.py
@@ -1,28 +1,37 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-
+import os
 import requests
 import json
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
 
 key = "599471b6ead24ab2914dd26f79c318ae"
 endpoint = "https://api.cognitive.microsofttranslator.com"
 location = "westeurope"
 
+endpoint2 = "https://amdocint.cognitiveservices.azure.com/"
+key2 = "003509088f534a798adc04cecc767e65"
+
 input_code = "pl"
 output_code = "en"
 text = ""
 translated_text = ""
+
 app = Flask(__name__)
 CORS(app)
 
-def translateTextAzure():
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def translate_text_azure():
     global translated_text
     global input_code
     global output_code
-    try:
-        route = "/translate?api-version=3.0&from"
 
-        route = route + "=" + input_code + "&to=" + output_code
+    try:
+        route = f"/translate?api-version=3.0&from={input_code}&to={output_code}"
 
         headers = {
             "Ocp-Apim-Subscription-Key": key,
@@ -34,23 +43,73 @@ def translateTextAzure():
         request_body = json.dumps(body)
         url = endpoint + route
         response = requests.post(url, headers=headers, data=request_body)
+
         if response.status_code == 200:
             data = json.loads(response.text)
             translations = data[0]['translations']
             translated_text = [item['text'] for item in translations][0]
         else:
             print("Error:", response.status_code, response.text)
+
     except KeyboardInterrupt:
         print("\nProgram stopped by the user.")
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route('/upload-file', methods=['POST'])
+def upload_file():
+    try:
+        file = request.files['file']
+
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return jsonify({'message': 'File uploaded successfully'}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/analyze-document', methods=['POST'])
+def analyze_document():
+    try:
+        file = request.files['file']
+
+        if file:
+            filename = file.filename
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+
+            document_analysis_client = DocumentAnalysisClient(
+                endpoint=endpoint2, credential=AzureKeyCredential(key2)
+            )
+
+            with open(file_path, "rb") as form_file:
+                poller = document_analysis_client.begin_analyze_document(
+                    "prebuilt-read", document=form_file
+                )
+
+            result = poller.result()
+            translated_text = result.content
+            delete_file(file_path)
+            return jsonify({'filename': filename, 'translated_text': translated_text}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+def extract_text_from_result(result):
+    try:
+        return " ".join([line.content for page in result.pages for line in page.lines])
+    except Exception as e:
+        return str(e)
+
+def delete_file(file_path):
+    try:
+        os.remove(file_path)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 @app.route("/get-languages")
 def get_languages():
     l_library = {}
-    url = endpoint + "/languages?api-version=3.0"
+    url = f"{endpoint}/languages?api-version=3.0"
 
     headers = {
         "Ocp-Apim-Subscription-Key": key,
@@ -61,7 +120,7 @@ def get_languages():
 
     if response.status_code == 200:
         if response:
-            data = json.loads(response.text)  # Parse the JSON string
+            data = json.loads(response.text)
 
             for scope in data.values():
                 for lang_code, lang_info in scope.items():
@@ -74,7 +133,7 @@ def get_languages():
 
 @app.route('/translate-text')
 def get_text():
-    translateTextAzure()
+    translate_text_azure()
     return jsonify(translated_text)
 
 @app.route('/set-text', methods=['POST'])
@@ -82,12 +141,14 @@ def set_text():
     global text
     global input_code
     global output_code
+
     try:
         data = request.get_json()
         text = data['text']
         input_code = data['inputLang']
         output_code = data['outputLang']
         return jsonify({'message': 'Data updated successfully'}), 200
+
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
--- TranslatorFront/src/components/TextTranslator/TextTranslator.tsx
+++ TranslatorFront/src/components/TextTranslator/TextTranslator.tsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import './TextTranslator.css'
 
-export default function TextTranslator({lang} : any) {
-    const [text, setText] = useState('')
+export default function TextTranslator({ lang }: any) {
+    const [text, setText] = useState('');
     const [retText, setRetText] = useState('');
-
     const [inputLang, setInputLang] = useState("en");
 
     const handleInput = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -17,51 +16,81 @@ export default function TextTranslator({lang} : any) {
         setOutputLang(event.target.value);
     }
 
+    const handleTextChange = (e: React.FormEvent<HTMLDivElement>) => {
+      setText(e.currentTarget.textContent || '');
+  }
+
+    const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const formData = new FormData();
+            formData.append('file', file);
+
+            try {
+              alert('Started file upload');
+                const response = await fetch('http://127.0.0.1:5000/analyze-document', {
+                    method: 'POST',
+                    body: formData,
+                });
+
+                const data = await response.json();
+
+                // Update the text field with the translated text
+                setText(data.translated_text);
+                // Notify the user that the file has been uploaded and processed
+                alert('File uploaded and processed successfully!');
+            } catch (error) {
+                console.error('Error uploading or processing file:', error);
+            }
+        }
+    };
+
     const translateText = () => {
-      fetch('http://127.0.0.1:5000/set-text', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({text, inputLang, outputLang}),
-      })
-        .then(response => response.json())
-        .then(data => {
-          fetch('http://127.0.0.1:5000/translate-text')
-          .then(response => response.json())
-          .then(data => {
-            setRetText(data);
-          })
-          .catch(error => {
-            console.log(error);
-          })
+        fetch('http://127.0.0.1:5000/set-text', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text, inputLang, outputLang }),
         })
-        .catch(error => {
-          console.error('Error:', error);
-        })
+            .then(response => response.json())
+            .then(data => {
+                fetch('http://127.0.0.1:5000/translate-text')
+                    .then(response => response.json())
+                    .then(data => {
+                        setRetText(data);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            })
     }
-  
+
     return (
-      <>
-        <div className="text-container">
-            <select className="text-language" style={{gridArea: "1 / 1"}} value={inputLang} onChange={handleInput}>
-                {Object.keys(lang).map((e) => (
-                    <option key={e} value={e}>
-                        {e +  ' - ' + lang[e]}
-                    </option>
-                ))}
-            </select>
-            <div className="text-field" style={{gridArea: "2 / 1"}} contentEditable onInput={(e) => setText(e.target.innerText)}></div>
-            <button onClick={translateText} style={{gridArea: "2 / 2"}}>Translate</button>
-            <select className="text-language" style={{gridArea: "1 / 3"}} value={outputLang} onChange={handleOutput}>
-                {Object.keys(lang).map((e) => (
-                    <option key={e} value={e}>
-                        {e +  ' - ' + lang[e]}
-                    </option>
-                ))}
-            </select>
-            <div className="text-field" style={{gridArea: "2 / 3"}}>{retText}</div>
-        </div>
-      </>
+        <>
+            <div className="text-container">
+                <select className="text-language" style={{ gridArea: "1 / 1" }} value={inputLang} onChange={handleInput}>
+                    {Object.keys(lang).map((e) => (
+                        <option key={e} value={e}>
+                            {e + ' - ' + lang[e]}
+                        </option>
+                    ))}
+                </select>
+                <div className="text-field" style={{ gridArea: "2 / 1"}} contentEditable onInput={handleTextChange}></div>
+                <input type="file" onChange={handleFileChange} style={{ gridArea: "3 / 1" }}/>
+                <button onClick={translateText} style={{ gridArea: "2 / 2" }}>Translate</button>
+                <select className="text-language" style={{ gridArea: "1 / 3" }} value={outputLang} onChange={handleOutput}>
+                    {Object.keys(lang).map((e) => (
+                        <option key={e} value={e}>
+                            {e + ' - ' + lang[e]}
+                        </option>
+                    ))}
+                </select>
+                <div className="text-field" style={{ gridArea: "2 / 3" }}>{retText}</div>
+            </div>
+        </>
     )
 }
